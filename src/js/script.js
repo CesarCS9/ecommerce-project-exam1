decafe7c77961e4c49754d70ede914f8538e1b3a
Cesar Castillo <cescastillo9@gmail.com>
2026-09-24 14:09:13 +0200

    refine mobile navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Burger menu
 const navBurger = document.querySelector('.nav-burger');
 const mobileMenu = document.querySelector('.nav-mobile-menu');
 
@@ -9,3 +10,34 @@ navBurger.addEventListener('click', () => {
     navBurger.setAttribute('aria-expanded', !isOpen);
     mobileMenu.classList.toggle('active');
 })
+
+// Close the mobile menu
+function closeMobileMenu() {
+  mobileMenu.classList.remove("active");
+  navBurger.setAttribute("aria-expanded", "false");
+}
+
+// Close the menu when clicking outside
+document.addEventListener("click", (event) => {
+  if (
+    mobileMenu.classList.contains("active") &&
+    !mobileMenu.contains(event.target) &&
+    !navBurger.contains(event.target)
+  ) {
+    closeMobileMenu();
+  }
+});
+
+// Close the menu when clicking a link
+const mobileLinks = mobileMenu.querySelectorAll("a");
+
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", closeMobileMenu);
+});
+
+// Close the menu when scrolling
+window.addEventListener("scroll", () => {
+  if (mobileMenu.classList.contains("active")) {
+    closeMobileMenu();
+  }
+});
